5739f8d2d390ff69df1fab90047d04ccb4e28bbf
khalid <khalidtwfik66@yahoo.com>
2023-10-31 18:24:05 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let list = document.getElementById('menu');
+let bar = document.getElementById('bar');
+list.style.display = 'none';
+
+
+
+   
+
+    function to() {
+        if (list.style.display == 'none') {
+           list.style.display='block'
+       }
+        else {
+            list.style.display='none'
+       }
+}
+let ko = document.getElementById('ko');
+
+function momo() {
+    ko.src='Untitled-1.pngjs1.png'
+}
+   
+
